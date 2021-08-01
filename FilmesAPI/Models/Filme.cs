ebdcc664d683d4cd24522d96dadcd7c620884dc2
rdruzian <rendruzian@gmail.com>
2021-08-01 12:05:42 -0300

    Adicionar arquivos de projeto.

---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPI.Models
+{
+    public class Filme
+    {
+        [Required(ErrorMessage = "O título é obrigatório")]
+        public string Titulo { get; set; }
+        
+        [Required(ErrorMessage = "O diretor é obrigatório")]
+        public string Diretor { get; set; }
+        
+        public string Genero { get; set; }
+        [Range(1, 600, ErrorMessage = "A druação não pode ser maior do que 600 minutos")]
+        public int Duracao { get; set; }
+    }
+}
